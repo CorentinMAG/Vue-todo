cb2107b041a4a8636324ef64f590b21041c23f3e
CorentinMAG <cor.mag91@gmail.com>
2021-11-26 20:02:19 +0100

    finished

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,33 @@
 <template>
-  <div>
-    <h1>{{ title }}</h1>
-    <Card>
-      <template #content>
-        <TodoInput/>
-        <TodoList v-if="length" :todos="filteredTodos"/>
-      </template>
-      <template #footer>
-        <TodoFilter :length="length" @filter="onFilterChange($event)" @clear="onClear"/>
-      </template>
-    </Card>
-  </div>
+<div>
+  <TodoList v-model="todos"/>
+  <button @click="addTodo">Ajouter</button>
+
+</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import TodoList from './components/TodoList.vue'
-import TodoInput from './components/TodoInput.vue'
-import TodoFilter from './components/TodoFilter.vue'
-import { CLEAR } from './utils/constantes'
-import { Todo } from './models/todo'
 
 export default defineComponent({
   name: 'App',
-  components: {
-    TodoFilter,
-    TodoList,
-    TodoInput
-  },
-  mounted () {
-    this.length = this.$store.getters.todoLength
-  },
   data () {
     return {
-      title: 'TODO LIST',
-      length: 0,
-      filter: 'all'
+      todos: [{ name: 'demo', completed: true }]
     }
+  },
+  components: {
+    TodoList
   },
   methods: {
-    onFilterChange (filter: string) {
-      this.filter = filter
-    },
-    onClear () {
-      this.$store.dispatch(CLEAR)
-    }
-  },
-  computed: {
-    filteredTodos (): Todo[] {
-      return this.$store.getters.filter(this.filter)
+    addTodo () {
+      this.todos.push({ name: 'Jean', completed: false })
     }
   }
 })
 </script>
 
 <style lang="scss">
-* {
-  box-sizing: border-box;
-}
-body, html {
-  font-size: 18px;
-  padding: 0;
-  margin: 0;
-  width: 100%;
-  height: 100vh;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#app {
-  height: inherit;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
+
 </style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,30 +1,120 @@
 <template>
-    <div>
-        <TodoItem v-for="todo in mytodos" :key="todo.id" :todo="todo"></TodoItem>
-    </div>
+  <section class="todoapp">
+    <header class="header">
+      <h1>Todos</h1>
+      <input type="text" class="new-todo" placeholder="Ajouter une tache" v-model="newTodo" @keyup.enter="addTodo">
+    </header>
+    <section class="main">
+      <input type="checkbox" class="toggle-all" id="toggle-all" v-model="allDone">
+      <label for="toggle-all">Mark all as complete</label>
+      <ul class="todo-list">
+        <li class="todo" v-for="todo in filteredTodos" :key="todo" :class="{completed: todo.completed, editing: todo === editing}">
+          <div class="view">
+            <input type="checkbox" v-model="todo.completed" class="toggle"/>
+            <label @dblclick="editTodo(todo)">{{ todo.name }}</label>
+            <button class="destroy" @click.prevent="deleteTodo(todo)"></button>
+          </div>
+          <input type="text" class="edit" v-model="todo.name" @keyup.enter="doneEdit" @blur="doneEdit" @keyup.esc="cancelEdit" v-focus="todo === editing">
+        </li>
+      </ul>
+    </section>
+    <footer class="footer" v-show="todos.length > 0">
+      <span class="todo-count"><strong>{{ remaining }}</strong> tâches à faire</span>
+      <ul class="filters">
+        <li><a href="#" :class="{selected: filter === 'all'}" @click.prevent="filter = 'all'">Toutes</a></li>
+        <li><a href="#" :class="{selected: filter === 'todo'}" @click.prevent="filter = 'todo'">A faire</a></li>
+        <li><a href="#" :class="{selected: filter === 'done'}" @click.prevent="filter = 'done'">Faites</a></li>
+      </ul>
+      <button v-show="completed" class="clear-completed" @click.prevent="deleteCompleted">Supprimer les taches finies</button>
+    </footer>
+  </section>
 </template>
-<script lang="ts">
-import { defineComponent, PropType } from 'vue'
-import { Todo } from '../models/todo'
-import TodoItem from './TodoItem.vue'
+
+<script lang="js">
+import Vue, { defineComponent } from 'vue'
 export default defineComponent({
-  name: 'TodoList',
   props: {
-    todos: {
-      type: Array as PropType<Todo[]>
+    modelValue: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  directives: {
+    focus (el, value) {
+      if (value) {
+        Vue.nextTick(() => {
+          el.focus()
+        })
+      }
     }
   },
   data () {
     return {
-      mytodos: this.todos
+      todos: this.modelValue,
+      newTodo: '',
+      filter: 'all',
+      editing: null,
+      oldTodo: null
     }
   },
-  components: {
-    TodoItem
+  watch: {
+    modelValue (value) {
+      this.todos = value
+    }
+  },
+  methods: {
+    addTodo () {
+      this.todos.push({ completed: false, name: this.newTodo })
+      this.newTodo = ''
+    },
+    deleteTodo (todo) {
+      this.todos = this.todos.filter(t => t !== todo)
+      this.$emit('update:modelValue', this.todos)
+    },
+    deleteCompleted () {
+      this.todos = this.todos.filter(t => !t.completed)
+      this.$emit('update:modelValue', this.todos)
+    },
+    editTodo (todo) {
+      this.oldTodo = todo.name
+      this.editing = todo
+    },
+    doneEdit () {
+      this.editing = null
+    },
+    cancelEdit () {
+      this.editing.name = this.oldTodo
+      this.doneEdit()
+    }
+  },
+  computed: {
+    remaining () {
+      return this.todos.filter(t => !t.completed).length
+    },
+    completed () {
+      return this.todos.filter(t => t.completed).length
+    },
+    filteredTodos () {
+      if (this.filter === 'todo') {
+        return this.todos.filter(t => !t.completed)
+      } else if (this.filter === 'done') {
+        return this.todos.filter(t => t.completed)
+      } else {
+        return this.todos
+      }
+    },
+    allDone: {
+      get () {
+        return this.remaining === 0
+      },
+      set (value) {
+        this.todos.forEach(t => { t.completed = value })
+      }
+    }
   }
 })
 </script>
 
-<style lang="sass">
-
-</style>
+<style lang="scss" src="./todos.scss"></style>
