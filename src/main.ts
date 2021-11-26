514596aae60b78e449038a7158112ae6e4e0ad3c
CorentinMAG <cor.mag91@gmail.com>
2021-11-27 00:00:47 +0100

    updated

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import store from './store'
-import PrimeVue from 'primevue/config'
-
-import 'primevue/resources/themes/lara-light-indigo/theme.css'
-import 'primevue/resources/primevue.min.css'
-import 'primeicons/primeicons.css'
-import 'primeflex/primeflex.css'
-import Card from 'primevue/card'
-import InputText from 'primevue/inputtext'
-import Button from 'primevue/button'
-import ToggleButton from 'primevue/togglebutton'
 
 createApp(App)
-  .use(store)
-  .use(PrimeVue)
-  .component('Card', Card)
-  .component('Input', InputText)
-  .component('Button', Button)
-  .component('ToggleButton', ToggleButton)
   .mount('#app')
